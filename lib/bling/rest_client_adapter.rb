5b2352d11d3e336866fd94eff6361eb2cc5d0a67
ViniRinaldis <vinireato@gmail.com>
2019-10-25 12:10:12 -0300

    remove logs

--- lib/bling/rest_client_adapter.rb
+++ lib/bling/rest_client_adapter.rb
@@ -2,9 +2,7 @@ module Bling
   class RestClientAdapter
 
     def self.http_get url, headers = {}
-      log "RestClientAdapter::http_get url: #{url} headers: #{headers}"
       response = RestClient.get(url, headers)
-      log "RestClientAdapter::http_get response: #{response.inspect}"
 
       OpenStruct.new(:status => response.code, :body => response.body)
 
@@ -13,9 +11,7 @@ module Bling
     end
     
     def self.http_post url, params = {}
-      log "RestClientAdapter::http_post url: #{url} params: #{params}"
       response = RestClient.post(url, params)
-      log "RestClientAdapter::http_post response: #{response.inspect}"
 
       OpenStruct.new(:status => response.code, :body => response.body)
 
